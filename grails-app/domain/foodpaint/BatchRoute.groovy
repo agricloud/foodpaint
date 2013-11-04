070f306dbbcd97fec64a2b754f91644c621fd267
Pipi <pipi@Pipis-MacBook-Air.local>
2013-11-04 11:50:32 +0800

    add batchRoute table

---
+++ grails-app/domain/foodpaint/BatchRoute.groovy
@@ -0,0 +1,29 @@
+package foodpaint
+
+class BatchRoute extends DefaultTable{
+
+	static belongsTo=[batch:Batch]
+	Workstation workstation
+	Operation operation
+	int sequence
+
+
+    /*
+    * 開始時間
+    */
+	Date startDate
+
+
+    /*
+    * 結束時間
+    */
+	Date endDate
+
+	 
+
+    static constraints = {
+    	sequence unique:'batch'
+    	startDate nullable:true
+    	endDate nullable:true
+    }
+}
---
+++ grails-app/domain/foodpaint/view/ManufactureOrderRouteView.groovy
@@ -0,0 +1,61 @@
+package foodpaint.view
+
+
+    /*
+    * 製令製程
+    */
+class ManufactureOrderRouteView implements Serializable{
+
+    Integer flag
+
+    /*
+    * 單別
+    */
+    String typeName
+
+
+    /*
+    * 單號
+    */
+    String name
+
+    /*
+    *  加工順序
+    */
+    int sequence
+
+    /*
+    * 製程代號
+    */
+    String operationName
+
+    /*
+    * 製程性質 1.廠內 2.託外
+    */
+    String type
+
+    /*
+    * 工作站/廠商代號
+    */
+    String makerName
+
+    static constraints = {
+
+    }
+
+    static mapping = {
+        datasource 'erp'
+        table 'SFCTA'
+        version false
+
+        id composite: ['typeName','name','sequence']
+        flag column: 'FLAG', sqlType:"numeric"
+        typeName column: 'TA001', sqlType: "nchar"
+        name column: 'TA002', sqlType: "nchar"
+        sequence  column: 'TA003', sqlType: "nchar"
+        operationName column: 'TA004', sqlType: "nchar"
+        type column: 'TA005', sqlType: "nchar"
+        makerName column: 'TA006', sqlType: "nchar"
+        
+    } 
+}
