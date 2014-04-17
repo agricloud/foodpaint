a75cbc199a345c883e2ac649fcb6d870c759ebeb
r85306474 <r85306474@gmail.com>
2014-04-17 19:40:52 +0800

    new

--- grails-app/domain/foodpaint/AccountancyPeriod.groovy
+++ grails-app/domain/foodpaint/AccountancyPeriod.groovy
@@ -1,52 +1,52 @@
-package foodpaint
-import grails.converters.*
+// package foodpaint
+// import grails.converters.*
 
 
-class AccountancyPeriod {
-    int importFlag = -1
+// class AccountancyPeriod {
+//     int importFlag = -1
 
 
-    /**
-     * 年度
-     */
-    Year  year 
+//     /**
+//      * 年度
+//      */
+//     Year  year 
     
-    /**
-     * 會計期間
-     */
-    int period
+//     /**
+//      * 會計期間
+//      */
+//     int period
 
-     /**
-     * 年度起始日期
-     */
-    Date startDayofYear
+//      /**
+//      * 年度起始日期
+//      */
+//     Date startDayofYear
 
-    /**
-     * 建立日期（自動欄位）
-     */
-    Date dateCreated
+//     /**
+//      * 建立日期（自動欄位）
+//      */
+//     Date dateCreated
 
-    /**
-     * 修改日期（自動欄位）
-     */
-    Date lastUpdated
+//     /**
+//      * 修改日期（自動欄位）
+//      */
+//     Date lastUpdated
 
 
-    /**
-     *備註
-     */
-    String remark
+//     /**
+//      *備註
+//      */
+//     String remark
 
-    static mapping = {
-        importFlag  defaultValue: -1
-    }
+//     static mapping = {
+//         importFlag  defaultValue: -1
+//     }
  
-    static constraints = {
-        sequence unique: true,
-        year  nullable:false
-        period nullable:false
-        startDay unique: true, blank: false
-        remark unique: true, blank: false
-	}
+//     static constraints = {
+//         sequence unique: true,
+//         year  nullable:false,
+//         period nullable:false,
+//         startDay unique: true, blank: false
+//         remark unique: true, blank: false
+// 	}
 	
-}
+// }
