2f6c677d463f5a91111dff3b1c6b9f043d07c211
r85306474 <r85306474@gmail.com>
2014-04-17 18:44:21 +0800

    new

---
+++ grails-app/domain/foodpaint/AccountancyDet.groovy
@@ -0,0 +1,102 @@
+package foodpaint
+import grails.converters.*
+
+
+class AccountancyDet {
+    int importFlag = -1
+
+    /**
+     * 所屬會計科目編號
+     */
+    String belongAccountTitleNumber
+    
+    /**
+     * 所屬會計科目名稱
+     */
+    String belongAccountTitleName
+
+
+    /**
+     * 會計科目級別
+     */
+    String  accountTitlelevel
+
+    /**
+     * 會計科目編號
+     */
+    String accountTitleNumber
+
+    /**
+     * 會計科目名稱
+     */ 
+    String accountTitleName
+
+     /**
+     * 會計科目別名
+     */ 
+    String accountTitleAlias
+
+     /**
+     * 會計科目性質
+     */ 
+    String accountNature
+
+    /**
+     * 資產損益別
+     */ 
+    String  balanceorIncomeType
+
+     /**
+     * 餘額借貸別
+     */ 
+    String remainofdebitcreditType
+
+     /**
+     * 報表借貸別
+     */ 
+    String debitcreditofTatementsType
+
+     /**
+     * 會計科目類別
+     */ 
+    String accountType
+
+    /**
+     * 建立日期（自動欄位）
+     */
+    Date dateCreated
+
+    /**
+     * 修改日期（自動欄位）
+     */
+    Date lastUpdated
+
+    int sequence
+    static belongsTo=[accountancy :Accountancy ]
+    /*
+
+    /**
+     *備註
+     */
+    String remark
+
+    static mapping = {
+        importFlag  defaultValue: -1
+    }
+ 
+    static constraints = {
+        accountTitlelevel nullable:false
+        belongAccountTitleNumber nullable:false
+        belongaccountTitleName nullable:false
+        accountTitleNumber unique: true, blank: false
+        accountTitleName nullable:false
+        accountTitleAlias nullable:true
+        accountNature nullable:true
+        balanceorIncomeType nullable:true
+        remainofdebitcreditType nullable:true
+        debitcreditofTatementsType nullable:true
+        remark unique: true, blank: false
+
+	}
+	
+}
---
+++ grails-app/domain/foodpaint/AccountancyPeriod.groovy
@@ -0,0 +1,52 @@
+package foodpaint
+import grails.converters.*
+
+
+class AccountancyPeriod {
+    int importFlag = -1
+
+
+    /**
+     * 年度
+     */
+    Year  year 
+    
+    /**
+     * 會計期間
+     */
+    int period
+
+     /**
+     * 年度起始日期
+     */
+    Date startDayofYear
+
+    /**
+     * 建立日期（自動欄位）
+     */
+    Date dateCreated
+
+    /**
+     * 修改日期（自動欄位）
+     */
+    Date lastUpdated
+
+
+    /**
+     *備註
+     */
+    String remark
+
+    static mapping = {
+        importFlag  defaultValue: -1
+    }
+ 
+    static constraints = {
+        sequence unique: true,
+        year  nullable:false
+        period nullable:false
+        startDay unique: true, blank: false
+        remark unique: true, blank: false
+	}
+	
+}
